563bf5f0fb25c85402e91578f5154b5d556297c4
Vaibhavi Shanbhag <vaibhavimshanbhag@gmail.com>
2026-07-01 22:57:42 +0530

    Remove test_gemini.py

--- backend_fixed/test_gemini.py
+++
@@ -1,7 +0,0 @@
-import sys 
-from config import settings 
-print('Python:', sys.version) 
-key = settings.GEMINI_API_KEY 
-print('KEY:', key[:8] if key else 'EMPTY') 
-import google.generativeai as genai 
-print('genai: OK') 
